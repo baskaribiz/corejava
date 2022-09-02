ff024c0cbe199653a26bbd929dc09d8c359f8e1e
baskaribiz <baskar.ibiz@gmail.com>
2022-09-02 21:59:02 +0530

    Initial Commit

---
+++ src/com/concepts/concurrentcollection/ConcurrentCollectionDemo.java
@@ -0,0 +1,41 @@
+package com.concepts.concurrentcollection;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentCollectionDemo {
+
+	public static ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<String, String>();
+	
+	public static void main(String[] args) {
+		
+		chm.put("101", "Kayal Kutti");
+		chm.put("102", "Ezhil");
+		
+		Runnable r = () -> {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				chm.put("103", "Palanivel");
+		};
+		Thread t = new Thread(r);
+		t.start();
+		
+		Set s = chm.keySet();
+		
+		Iterator<String> itr = s.iterator();
+		
+		while(itr.hasNext()) {
+			
+			System.out.println(chm.get(itr.next().toString()));
+			
+		}
+
+		System.out.println(chm);
+	}
+
+}
